1130be48554d123b3ef8017ab09b3c8f8517c0b4
Mohamad <husseini.mel@gmail.com>
2022-02-09 00:37:08 -0300

    Remove test comments

--- lib/pordle.ex
+++ lib/pordle.ex
@@ -18,8 +18,7 @@ defmodule Pordle do
 
   """
   def create_game(opts \\ []) do
-    # puzzle = Dictionary.get()
-    puzzle = "skill"
+    puzzle = Dictionary.get()
 
     opts
     |> Keyword.put(:answer, puzzle)
@@ -37,13 +36,13 @@ defmodule Pordle do
 
   """
   def put_player_move(server, word) do
-    # case Dictionary.is_entry(word) do
-    #   {:ok, _word} ->
-    GenServer.call(server, {:put_player_move, word})
+    case Dictionary.is_entry(word) do
+      {:ok, _word} ->
+        GenServer.call(server, {:put_player_move, word})
 
-    #   {:error, error} ->
-    #     {:error, error}
-    # end
+      {:error, error} ->
+        {:error, error}
+    end
   end
 
   @doc """
